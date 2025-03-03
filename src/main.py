b8023befc6538b86cc3fbe7b60e340faae8e1223
mmarchetti <mikemarchetti95@yahoo.it>
2025-03-03 12:08:33 +0100

    Aggiornato main.py: -Fixato import dei metodi

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from gui import FileScannerApp
-from update_checker import check_for_update
+from update_checker import check_version
 
 if __name__ == "__main__":
-    check_for_update()  # Controlla la versione prima di avviare la GUI
+    check_version()  # Controlla la versione prima di avviare la GUI
 
     app = QApplication(sys.argv)
     window = FileScannerApp()
