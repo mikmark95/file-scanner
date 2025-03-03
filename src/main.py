49e60d76660bf44035fe34c0ba4626720c152e92
mmarchetti <mikemarchetti95@yahoo.it>
2025-03-03 10:49:55 +0100

    Aggiornato file main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
 # Avvio dell'applicazione
+
+# main.py
+import sys
+from PyQt6.QtWidgets import QApplication
+from gui import FileScannerApp
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = FileScannerApp()
+    window.show()
+    sys.exit(app.exec())
