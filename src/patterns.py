b797096a783e90fc10415b3757665266850a4e0f
mmarchetti <mikemarchetti95@yahoo.it>
2025-03-03 10:47:24 +0100

    Aggiornato file patterns.py

--- src/patterns.py
+++ src/patterns.py
@@ -1,1 +1,29 @@
 # Funzioni per la gestione dei pattern regex
+
+# patterns.py
+import re
+import random
+
+contatore = 0
+
+
+def azione(pattern, entry_path):
+    global contatore
+    match pattern:
+        case "Anno-Mese-Giorno-Orario":
+            regex = r"\d*-\d*-\d*-\d*"
+        case "Anno-Mese-Giorno":
+            regex = r"\d*-\d*-\d*"
+        case "Numerico":
+            regex = r"\\(\d{1,2}?)\\"
+        case "Indice progressivo":
+            contatore += 1
+            return str(contatore)
+        case "Indice random":
+            return str(random.randint(1, 1000)).zfill(4)
+        case "Nessuno":
+            return 'vuoto'
+        case _:
+            return ''
+
+    return re.findall(regex, entry_path)[0] if re.findall(regex, entry_path) else ''
