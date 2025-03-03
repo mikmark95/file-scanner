7ca634c283ec21fdae9090cebfa2eef801da3bb3
mmarchetti <mikemarchetti95@yahoo.it>
2025-03-03 12:27:48 +0100

    Aggiornato main.py: -Fix Typo

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ from gui import FileScannerApp
 from update_checker import check_version
 
 if __name__ == "__main__":
-    check_version()  # Controlla la versione prima di avviare la GUI
 
     app = QApplication(sys.argv)
     window = FileScannerApp()
     window.show()
+    check_version()  # Controlla la versione prima di avviare la GUI
     sys.exit(app.exec())
