31356c3b5f882a0dbc92203a837e26a0e9793e7c
mmarchetti <mikemarchetti95@yahoo.it>
2025-03-03 11:26:13 +0100

    Aggiornato main.py: -Aggiunto il comtrollo della versione alla avvio

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
-# Avvio dell'applicazione
-
 # main.py
 import sys
 from PyQt6.QtWidgets import QApplication
 from gui import FileScannerApp
+from update_checker import check_for_update
 
 if __name__ == "__main__":
+    check_for_update()  # Controlla la versione prima di avviare la GUI
+
     app = QApplication(sys.argv)
     window = FileScannerApp()
     window.show()
