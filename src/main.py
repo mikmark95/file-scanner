acf82cf18dd36731bdccc46f8bbd2d2ff4ab1d8a
mmarchetti <mikemarchetti95@yahoo.it>
2025-03-04 09:47:34 +0100

    Aggiornato il main.py: -Refactor della funzione center_widget() da main.py a file_utils.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import QTimer
 from gui import FileScannerApp
+from src.file_utils import center_widget
 from update_checker import check_version
 from splash_screen import SplashScreen  # Importa la nuova classe SplashScreen
 
@@ -9,17 +10,11 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     # Funzione per centrare un widget sullo schermo
-    def center_widget(widget):
-        screen = app.primaryScreen()
-        screen_geometry = screen.availableGeometry()
-        widget_geometry = widget.frameGeometry()
-        widget_geometry.moveCenter(screen_geometry.center())
-        widget.move(widget_geometry.topLeft())
 
     # Crea lo splash screen
     splash = SplashScreen()
     splash.show()
-    center_widget(splash)
+    center_widget(app,splash)
 
     # Funzione per avviare la finestra principale dopo 3 secondi
     def start_main():
@@ -29,7 +24,7 @@ if __name__ == "__main__":
         main_window = FileScannerApp()
 
         # Centra la finestra principale prima di mostrarla
-        center_widget(main_window)
+        center_widget(app,main_window)
 
         # Mostra la finestra principale
         main_window.show()
