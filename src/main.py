e5299663115d410fa84e0f48e5990b3b894353e7
mmarchetti <mikemarchetti95@yahoo.it>
2025-03-06 13:06:35 +0100

    Aggiornato main.py: - Reformatting posizione funzioni

--- src/main.py
+++ src/main.py
@@ -6,6 +6,20 @@ from file_utils import center_widget
 from update_checker import check_version
 from splash_screen import SplashScreen  # Importa la nuova classe SplashScreen
 
+
+def start_main():
+    global main_window
+    splash.close()  # Chiude manualmente lo splash screen
+    check_version()  # Eseguiamo il controllo della versione prima di aprire la GUI
+    main_window = FileScannerApp()
+
+    # Centra la finestra principale prima di mostrarla
+    center_widget(app, main_window)
+
+    # Mostra la finestra principale
+    main_window.show()
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
@@ -14,20 +28,9 @@ if __name__ == "__main__":
     # Crea lo splash screen
     splash = SplashScreen()
     splash.show()
-    center_widget(app,splash)
+    center_widget(app, splash)
 
     # Funzione per avviare la finestra principale dopo 3 secondi
-    def start_main():
-        global main_window
-        splash.close()  # Chiude manualmente lo splash screen
-        check_version()  # Eseguiamo il controllo della versione prima di aprire la GUI
-        main_window = FileScannerApp()
-
-        # Centra la finestra principale prima di mostrarla
-        center_widget(app,main_window)
-
-        # Mostra la finestra principale
-        main_window.show()
 
     # Avvia la GUI dopo 3 secondi
     QTimer.singleShot(3000, start_main)
